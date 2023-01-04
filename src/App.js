766ec1ee845403191a29a77c39390c64db5a6285
khaled Reza <khaledreza@gmail.com>
2023-01-04 18:33:36 +0100

    added darkmode lightmode using manual color option, added top bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+import { ColorModeContext, useMode } from "./theme";
+import { Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard/Dashboard";
+import Team from "./scenes/team/Team";
+import Invoices from "./scenes/invoices/Invoices";
+import Contacts from "./scenes/contacts/Contacts";
+import Bar from "./scenes/bar/Bar";
+import Form from "./scenes/form/Form";
+import Line from "./scenes/line/Line";
+import Pie from "./scenes/pie/Pie";
+import FAQ from "./scenes/faq/Faq";
+import Geography from "./scenes/geography/Geography";
+
 function App() {
-  return <div className="app">Hello</div>;
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
